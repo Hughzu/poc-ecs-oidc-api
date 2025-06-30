a0621875b3c02d8943e18b1eb4e29cada5303592
Hughes <hughesstiernon@gmail.com>
2025-06-30 20:44:53 +0200

    clean up

--- terraform/app/modules/ecs/main.tf
+++ terraform/app/modules/ecs/main.tf
@@ -385,30 +385,30 @@ resource "aws_ecs_task_definition" "app" {
 }
 
 # ECS Service
-resource "aws_ecs_service" "app" {
-  name            = "${var.project_name}-${var.app_name}-service"
-  cluster         = aws_ecs_cluster.main.id
-  task_definition = aws_ecs_task_definition.app.arn
-  desired_count   = var.desired_count
-  launch_type     = "EC2"
+# resource "aws_ecs_service" "app" {
+#   name            = "${var.project_name}-${var.app_name}-service"
+#   cluster         = aws_ecs_cluster.main.id
+#   task_definition = aws_ecs_task_definition.app.arn
+#   desired_count   = var.desired_count
+#   launch_type     = "EC2"
 
 
-  load_balancer {
-    target_group_arn = aws_lb_target_group.app.arn
-    container_name   = var.app_name
-    container_port   = var.container_port
-  }
+#   load_balancer {
+#     target_group_arn = aws_lb_target_group.app.arn
+#     container_name   = var.app_name
+#     container_port   = var.container_port
+#   }
 
-  depends_on = [
-    aws_lb_listener.app,
-    aws_autoscaling_group.ecs_asg
-  ]
+#   depends_on = [
+#     aws_lb_listener.app,
+#     aws_autoscaling_group.ecs_asg
+#   ]
 
-  tags = {
-    Name = "${var.project_name}-${var.app_name}-service"
-  }
+#   tags = {
+#     Name = "${var.project_name}-${var.app_name}-service"
+#   }
 
-  lifecycle {
-    ignore_changes = [desired_count]
-  }
-}
+#   lifecycle {
+#     ignore_changes = [desired_count]
+#   }
+# }
